80e26dca55603935470ecea2ab16e4855a35b2d9
Melimet <erkka.rahikainen@gmail.com>
2022-10-13 16:15:03 +0300

    9.10

---
+++ patientor_backend/src/routes/diagnoses.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { getDiagnoses } from "../services/diagnoseService";
+const diagnoseRouter = express.Router()
+
+diagnoseRouter.get('/', (_req, res) => {
+  res.send(getDiagnoses())
+})
+
+export { diagnoseRouter }
