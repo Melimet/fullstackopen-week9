855fce0051cc89b9ff3472152cb94af2ab78456d
Melimet <erkka.rahikainen@gmail.com>
2022-10-16 17:49:29 +0300

    9.19

--- patientor_backend/src/types.ts
+++ patientor_backend/src/types.ts
@@ -4,16 +4,46 @@ enum Gender {
   Other = "other"
 }
 
+enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
 interface Diagnose {
   code: string
   name: string
   latin?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Entry{
-
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare"
+  employerName: string
+  sickLeave?: {
+    startDate: string
+    endDate: string
+  }
 }
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital'
+  discharge: {
+    date: string
+    criteria: string
+  }
+}
+
+type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 
 interface Patient {
   id: string
@@ -25,9 +55,17 @@ interface Patient {
   entries: Entry[]
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
 type PatientWithoutSsn = Omit<Patient, 'ssn' | 'entries'>;
 type NewPatient = Omit<Patient, 'id' >;
 
-export { Diagnose, Patient, PatientWithoutSsn, NewPatient, Gender, Entry};
+export { Diagnose, Patient, PatientWithoutSsn, NewPatient, Gender, Entry, HealthCheckEntry};
 
 
